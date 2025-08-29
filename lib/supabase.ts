770f481119b415b80d1604e74b4e8aad27e8d8c1
ChinitoOjeda <ojedawalterj@gmail.com>
2025-08-29 00:09:30 -0300

    ahora el cliente de supabase solo está disponible del lado del servidor

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,20 +1,10 @@
-import { createBrowserClient, createServerClient } from "@supabase/ssr"
-import { cookies } from "next/headers"
+import 'server-only';
+import { createClient } from '@supabase/supabase-js';
 
-// Cliente para el navegador
-export function createClient() {
-  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-}
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_SECRET_SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    auth: { persistSession: false },
+  }
+);
 
-// Cliente para el servidor
-export function createServerSupabaseClient() {
-  const cookieStore = cookies()
-
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-    cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value
-      },
-    },
-  })
-}
