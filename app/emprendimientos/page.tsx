ae294f6b532c17c6a063e493ff0468c72699f51d
ChinitoOjeda <ojedawalterj@gmail.com>
2025-09-13 13:50:10 -0300

    implemento obtencion de emprendimientos en pagina de emprendimientos

--- app/emprendimientos/page.tsx
+++ app/emprendimientos/page.tsx
@@ -1,13 +1,18 @@
 import { Navigation } from "@/components/navigation"
 import { ProjectsGrid } from "@/components/projects-grid"
 import { ProjectsHero } from "@/components/projects-hero"
+import { getProjects } from "@/domain/Projects"
+import { Project } from "@/types/Project"
 
-export default function EmprendimientosPage() {
+export default async function EmprendimientosPage() {
+
+  const allProjects = await getProjects() as Project[];
+
   return (
     <main className="min-h-screen bg-background">
       <Navigation />
       <ProjectsHero />
-      <ProjectsGrid />
+      <ProjectsGrid allProjects={allProjects} />
     </main>
   )
 }
