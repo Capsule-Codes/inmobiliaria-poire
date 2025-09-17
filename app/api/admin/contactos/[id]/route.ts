5eeac64662ce4f5d83906bf54ed91449e0272967
ChinitoOjeda <ojedawalterj@gmail.com>
2025-09-17 14:25:36 -0300

    agrego funcionalidad de actualizacion y borrado de contactos

---
+++ app/api/admin/contactos/[id]/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { deleteContact, updateContactStatus } from "@/domain/contact";
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+
+    try {
+
+        const { id } = await params;
+        await deleteContact(id);
+        return NextResponse.json({ ok: true }, { status: 200 });
+    } catch (err: any) {
+        const status = 500;
+        return NextResponse.json(
+            { message: err.message, issues: err.issues },
+            { status }
+        );
+    }
+}
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+
+    try {
+        const { id } = await params;
+        const { status } = await req.json();
+        const updatedContact = await updateContactStatus(id, status);
+        return NextResponse.json(updatedContact, { status: 200 });
+    } catch (err: any) {
+        const status = 500;
+        return NextResponse.json(
+            { message: err.message, issues: err.issues },
+            { status }
+        );
+    }
+}
